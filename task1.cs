8d1563c05514877421e5f68c649631b2549eeed9
T-Pavankumar15 <101107349+T-Pavankumar15@users.noreply.github.com>
2022-03-18 16:57:16 +0530

    Add files via upload

---
+++ task1.cs
@@ -0,0 +1,22 @@
+using System;
+class Cons
+{ 
+	int num1;
+	public Cons()
+	{
+	Console.WriteLine("Name and age");
+	num1=22;
+ 
+	}
+	public Name()
+	{
+	Console.WriteLine("Name: Pavankumar");
+	}
+public static void Main()
+{ 
+ Cons obj= new Cons();
+ obj.Name();
+ Console.WriteLine("Age:" +(obj.num1));
+
+}
+}
