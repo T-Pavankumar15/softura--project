8250f2636ce872f8f7b54792aee6c8c73b5d0bb8
T-Pavankumar15 <101107349+T-Pavankumar15@users.noreply.github.com>
2022-03-21 16:50:43 +0530

    Add files via upload

---
+++ task1,21.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Cfile3
+    {
+        public static void Main()
+        {
+            FileStream fs = new FileStream("F:\\softura\\day11\\employ.txt",
+            FileMode.OpenOrCreate,FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write("Empolyee Details\n");
+            bw.Write("Empoy id:1234\n");
+            bw.Write("Name:pavankumar\n");
+            bw.Write("Gender:male\n");
+            bw.Write("Age:22\n");
+            bw.Flush();
+            fs.Close();
+            FileStream fs1 = new FileStream("F:\\softura\\day11\\employ.txt",
+          FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            BinaryReader br = new BinaryReader(fs1);
+            string str = br.ReadString();
+            Console.WriteLine(str);
+           string str1 = br.ReadString();
+            Console.WriteLine(str1);
+            string str4 = br.ReadString();
+            Console.WriteLine(str4);
+            string str2 = br.ReadString();
+            Console.WriteLine(str2);
+            string str3 = br.ReadString();
+            Console.WriteLine(str3);
+            fs1.Close();
+            Console.Read();
+
+        }
+    }
+}
---
+++ task2,21.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class task
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("F:\\softura\\Dessert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Tea");
+            str.Add("Coffee");
+            str.Add("Dosa");
+            str.Add("vada");
+            str.Add("Lichi Mousse");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("F:\\Softura\\Dessert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"F:\Softura\Dessert.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
