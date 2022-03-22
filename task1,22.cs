59d29fa0b5ebaa05ef523c41141f116599ef1b4c
T-Pavankumar15 <101107349+T-Pavankumar15@users.noreply.github.com>
2022-03-22 16:16:48 +0530

    Add files via upload

---
+++ task1,22.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsoleApp2
+{
+
+    class task2
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("F:\\softura\\Dessert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("Tea");
+            str.Add("Coffee");
+            str.Add("Dosa");
+            str.Add("vada");
+            str.Add("Lichi Mousse");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("F:\\Softura\\Dessert.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+           
+            DirectoryInfo di = new DirectoryInfo(@"F:\Softura\Dessert.txt");
+            Console.WriteLine("Fullname:" + di.FullName);
+            Console.WriteLine("Creation time" + di.CreationTime);
+            Console.WriteLine("Last Acess" + di.LastAccessTime);
+            
+            Console.Read();
+        }
+    }
+
+}
+
---
+++ task2,22.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    public class Employee
+    {
+        public int ID =123;
+        public string Name="Pavankumar";
+        public string Gender="M";
+        public int Age=22;
+    }
+    class task1
+    {
+        public void Ser()
+        {
+            Employee emplo = new Employee();
+            FileStream fs = new FileStream("Employee", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            SoapFormatter Sf1 = new SoapFormatter();
+            Sf1.Serialize(fs, emplo);
+            fs.Close();
+        }
+        public void DeSer2Data()
+        {
+            Employee emplo = new Employee();
+            FileStream fs = new FileStream("Employee", FileMode.Open, FileAccess.Read);
+            SoapFormatter sf = new SoapFormatter();
+            Employee emp = (Employee)sf.Deserialize(fs);
+            Console.WriteLine(emp.ID);
+            Console.WriteLine(emp.Name);
+            Console.WriteLine(emp.Gender);
+            Console.WriteLine(emp.Age);
+
+        }
+        public static void Main()
+        {
+            task1 sobj = new task1();
+            sobj.Ser();
+            sobj.DeSer2Data();
+            Console.Read();
+        }
+    }
+}
