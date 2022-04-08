11ba8874e4b57b2dd4e2651af4d7d833e986d4b9
T-Pavankumar15 <101107349+T-Pavankumar15@users.noreply.github.com>
2022-04-08 17:27:59 +0530

    Add files via upload

---
+++ task(8-4-22).cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace linq
+{
+    class task
+    {
+		public static void Main()
+		{
+			int eyes = 52, Legs = 80;
+
+			int count = 0;
+			int count1 = 0;
+
+			count = 2 * (eyes) - (Legs);
+			count = count / 2;
+
+			Console.WriteLine("No of Ducks:" + count);
+
+			count1 = (eyes/2) - count;
+			Console.WriteLine("No of Goats:" + count1);
+		}
+	}
+}
+
